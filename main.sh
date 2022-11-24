d8944ead59673cecfc187d98062ee989e61caa09
hujiejeff <hujiejeff@163.com>
2022-11-24 10:28:27 +0800

    Add aria2c support
    Add aria2c support

--- main.sh
+++ main.sh
@@ -1,7 +1,9 @@
-# rm -rf alist* data/ #Uncomment this line to update
+# rm -rf alist* data/ #Uncomment this line if you want update
 if [ ! -f "alist" ];then
   curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
   tar -zxvf alist.tar.gz
   rm -f alist.tar.gz
+  mv alist-linux-musl-amd64 alist
 fi
-./alist server --no-prefix
+
+aria2c --enable-rpc --rpc-allow-origin-all --disable-ipv6& ./alist server --no-prefix
